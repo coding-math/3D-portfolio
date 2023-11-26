cdc2934d15433217ec028ea9cf973c29797ddfa9
matheuxito <matheus.souza28042001@gmail.com>
2023-11-26 03:08:17 -0300

    chore: update import and exports of image files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import backend from './backend.png';
-import creator from './creator.png';
 import react from './react.png';
 import web from './web.png';
 import github from './github.png';
@@ -9,31 +8,28 @@ import close from './close.svg';
 
 import css from './tech/css.png';
 import docker from './tech/docker.png';
-import figma from './tech/figma.png';
+import kubernetes from './tech/kubernetes.png';
+import django from './tech/django.png';
 import git from './tech/git.png';
 import html from './tech/html.png';
 import javascript from './tech/javascript.png';
-import mongodb from './tech/mongodb.png';
+import postgresql from './tech/postgresql.png';
 import nodejs from './tech/nodejs.png';
 import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
 import tailwind from './tech/tailwind.png';
 import typescript from './tech/typescript.png';
 import threejs from './tech/threejs.svg';
+import blackgithub from './tech/github_black.png';
 
-import meta from './company/meta.png';
-import shopify from './company/shopify.png';
-import starbucks from './company/starbucks.png';
-import tesla from './company/tesla.png';
+import akaer from './company/akaer.png';
+import unifesp from './company/unifesp.png';
 
-import carrent from './carrent.png';
-import jobit from './jobit.png';
-import tripguide from './tripguide.png';
+import yolo from './yolo.png';
+import cgprojects from './cgprojects.png';
 
 export {
   logo,
   backend,
-  creator,
   react,
   web,
   github,
@@ -41,22 +37,20 @@ export {
   close,
   css,
   docker,
-  figma,
+  django,
+  kubernetes,
   git,
+  blackgithub,
   html,
   javascript,
-  mongodb,
+  postgresql,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  akaer,
+  unifesp,
+  yolo,
+  cgprojects,
 };
