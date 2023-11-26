d7a0a6cfda72efa0986e52466b3bbe93667adb55
matheuxito <matheus.souza28042001@gmail.com>
2023-11-26 18:50:42 -0300

    refactor: update margin in card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full">
+  <Tilt className="xs:w-[250px] w-full mx-auto sm:mx-0">
     <motion.div
       variants={fadeIn('right', 'spring', 0.75 * index, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
